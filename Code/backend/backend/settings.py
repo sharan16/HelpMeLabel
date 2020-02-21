d78c71f2e20865bc9ac77337fa94a7a3409613e7
Sharan <kingsharan02161999@gmail.com>
2020-02-20 22:29:09 -0500

    implemented postgresql database, and did initial migrations

--- Code/backend/backend/settings.py
+++ Code/backend/backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edwzyzlc',
+        'USER' : 'edwzyzlc',
+        'PASSWORD' : 'ziG9GhOnTEPLAYnnmiLjNbd8S5g5T3Ia',
+        'HOST' : 'rajje.db.elephantsql.com',
+        'PORT' : '5432'
     }
 }
 
