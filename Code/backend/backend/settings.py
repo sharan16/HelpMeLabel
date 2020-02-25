a47600af31b2b420b890f5f9c758b193f0ace00c
TN <32180978+tharseken@users.noreply.github.com>
2020-02-24 22:16:18 -0500

    Front end start code (#1)

--- Code/backend/backend/settings.py
+++ Code/backend/backend/settings.py
@@ -127,5 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # we whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = [
-        'https://localhost:3000'
+        'https://localhost:3000',
+        'http://localhost:3000'
 ]
