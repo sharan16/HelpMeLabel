717c94ffb911d1306b56939f9c8364fa95e1a54d
Sharan <kingsharan02161999@gmail.com>
2020-02-27 19:08:36 -0500

    starter images model

--- Code/backend/labeler/models.py
+++ Code/backend/labeler/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Todo(models.Model):
-      title = models.CharField(max_length=120)
-      description = models.TextField()
-      completed = models.BooleanField(default=False)
+  title = models.CharField(max_length=120)
+  description = models.TextField()
+  completed = models.BooleanField(default=False)
 
-      def _str_(self):
-        return self.title
+  def _str_(self):
+    return self.title
+
+class Image(models.Model):
+  image_url = models.CharField(max_length=256)
+  local_file_name = models.CharField(max_length=128)
+  possible_labels = ArrayField(models.CharField(max_length=20, blank=True))
+
+  def _str_(self):
+    return self.local_file_name
