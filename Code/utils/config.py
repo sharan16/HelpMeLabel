202fbf4041ee52c7a8c3712378ae69c325de220f
TN <32180978+tharseken@users.noreply.github.com>
2020-04-19 16:49:16 -0400

    Script to simultaneously create AWS S3 bucket image instance and Post… (#3)
    * Script to simultaneously create AWS S3 bucket image instance and PostgreSQL object/instance.
    
    * Added check to make sure image added to AWS before adding to PostgreSQL.

---
+++ Code/utils/config.py
@@ -0,0 +1,8 @@
+BUCKET_NAME = 'labeler-images'
+ACCESS_KEY = 'AKIAJTVSX3LRCLCQGNWA'
+SECRET_KEY = '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'
+
+HOST = 'rajje.db.elephantsql.com'
+DB_NAME = 'edwzyzlc'
+USER = 'edwzyzlc'
+PASSWORD = 'ziG9GhOnTEPLAYnnmiLjNbd8S5g5T3Ia'
