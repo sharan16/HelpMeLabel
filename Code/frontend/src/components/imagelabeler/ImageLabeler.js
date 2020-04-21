6e1792c41374b47054444a02ec5308e3b40e1dd5
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2020-04-21 18:46:20 -0400

    Added functionality to scroll through images.

--- Code/frontend/src/components/imagelabeler/ImageLabeler.js
+++ Code/frontend/src/components/imagelabeler/ImageLabeler.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios' 
 import PropagateLoader from "react-spinners/PropagateLoader";
 import { css } from "@emotion/core";
+import IconButton from '@material-ui/core/IconButton';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Grow from '@material-ui/core/Grow';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 const ImageLabeler = () => {
 
@@ -9,36 +14,75 @@ const ImageLabeler = () => {
   	let [loading, setLoading] = useState(true)
 
 	const override = css`
-		position: absolute;
-		top: 30%;
 		left: 50%;
 	`;
 
+	const useStyles = makeStyles(theme => ({
+		root: {
+		  height: 450,
+		  display: "flex",
+		  flexDirection: "column",
+		  justifyContent: "center"
+		},
+		naviagateNext:{
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+		}
+	  }));
 
 	useEffect(async () => {
+		loadNextImage()
+	}, []);
+
+	let handleNavigateNext =  () => {
+		loadNextImage()
+	}
+
+	let loadNextImage = async() => {
+		setLoading(true)
 		let res = await axios.get('api/images/get_unlabeled_image/')
 		setImageURL(res.data.image_url)
 		setLoading(false)
-	}, []);
+	}
+
+	const classes = useStyles();
 
 	return (
 		<Fragment>
-			<div className = "label-image-header">
-				<h1>
-					Label This Image
-				</h1>
-			</div>
-			{ loading ?
-				<PropagateLoader 
-				css={override}
-				size={20}
-				color={"#5197e6"}
-				/>
-			:
-				<div className = "label-image">
-					<img src = {imageURL} width="42%" height="42%"/>
-				</div>
-			}
+			<Grid container spacing={4}>
+				<Grid item xs={12}>
+					<div className = "label-image-header">
+						<h1>
+							Label This Image
+						</h1>
+					</div>
+        		</Grid>
+				<Grid item xs={4}>
+					<div>
+					</div>
+        		</Grid>
+				<Grid className = {classes.root} item xs={4}>
+					{ loading ?
+						<PropagateLoader 
+						css={override}
+						size={20}
+						color={"#5197e6"}
+						/>
+					:
+						<Grow in ={true} timeout= {1000}>
+						<div className = "label-image">
+							<img className = "responsive-image" src = {imageURL}/>
+						</div>
+						</Grow>
+					}
+        		</Grid>
+				<Grid className = {classes.naviagateNext} item xs={1}>
+					<IconButton color="primary" aria-label="view next image" onClick = {() => {handleNavigateNext()}}>
+						<NavigateNextIcon />
+					</IconButton>
+        		</Grid>
+			</Grid>
 		</Fragment>
   	)
 }
