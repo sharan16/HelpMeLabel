99af63b3126887c06a6514e04f5a96d658816461
Shawn <kingsharan02161999@gmail.com>
2020-04-22 22:29:39 -0400

    Shan/ch26/bg task setup (#7)
    * background task jobs
    
    * background decorator
    
    * include lock file

---
+++ Code/backend/labeler/tasks.py
@@ -0,0 +1,9 @@
+from background_task import background
+from .models import Image
+
+
+@background()
+def reset_image_retrieve():
+    for image in Image.objects.all():
+        image.is_sent = False
+        image.save()
